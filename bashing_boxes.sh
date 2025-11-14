5c611c030ba8a2dbb97a62c6de4717fbca0c2766
BigNasXx <nassifak09@gmail.com>
2025-11-14 10:39:17 -0600

    Fixes to minor issues in code

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -6,10 +6,9 @@ datafolder="/home/bignasx/Bashing_Boxes/data"
 array_storage="/home/bignasx/Bashing_Boxes/data/array_storage" 
 maxnum="30"
 
-
-
-
-# prints the menu
+# ===============================================
+# Prints the main menu options for the user
+# ===============================================
 print_menu()
 {
 	echo "
@@ -26,16 +25,21 @@ print_menu()
  ||	 7) Load an array
  ||	 8) List saved arrays
  ||	 9) Delete an array
- ||	10) Quit
+ ||	10) Generate Random Box
+ ||	11) Quit
 	"
 }
 
-# Gets user input and stores it in user_input
+# ===============================================
+# Prompts user for input and stores it in user_input
+# ===============================================
 get_user_input(){
 	read -p "Enter menu selection: " user_input
 }
 
-# Prints a specific index from the current array
+# ===============================================
+# Prints a specific item in the array based on user index
+# ===============================================
 print_an_index_in_ccurrent_array()
 {
 	echo " What index do you want to print"
@@ -43,39 +47,22 @@ print_an_index_in_ccurrent_array()
 	
 	if [[ $user_input =~ [0-9]+$  && $user_input -ge 0 && $user_input -le ${#Objects[@]}-1 ]]; then	
 		echo ${Objects[$user_input]}
-		
 	else {
 			echo Input a Valid Input
 			print_an_index_in_ccurrent_array
 		}
 	fi
 	menu_selection
-
 }
 
-# Removes an item from the array at a specific index
-remove_item_at_x_index()
-{
-	echo what Item do you want to remove
-	get_user_input
-	if [[ $user_input =~ [0-9]+$ && $user_input -le ${#Objects[@]} && $user_input -ge 0 ]]; then
-		echo  "${Objects[$user]} deleted"
-		unset 'Objects[$user_input]'
-		Objects=("${Objects[@]}")
-		echo "Showing List
-		=====================
-		"
-		echo "${Objects[@]}"
-    else
-	{
-		echo Input Valid index
-		remove_item_at_x_index
-	}
-	fi
-	menu_selection
-}
+# ===============================================
+# Removes an object from the array at a given index
+# ===============================================
 
-# Prints all items in the array
+
+# ===============================================
+# Prints all items currently in the Objects array
+# ===============================================
 print_list()
 {
 	for item in "${!Objects[@]}";  do
@@ -84,7 +71,9 @@ print_list()
 	menu_selection
 }
 
-# Removes the last item in the array
+# ===============================================
+# Removes the last element in the Objects array
+# ===============================================
 remove_last_index()
 {
 	echo ${Objects[-1]} deleted
@@ -93,7 +82,9 @@ remove_last_index()
 	menu_selection
 }
 
-# Adds a new item to the array
+# ===============================================
+# Adds a new item entered by user to the array
+# ===============================================
 add_an_item_to_array()
 {
 	echo What item would you want to add
@@ -103,47 +94,34 @@ add_an_item_to_array()
 	menu_selection
 }
 
-# Displays the main menu and handles user choices
+# ===============================================
+# Displays menu and handles the user's choice
+# ===============================================
 menu_selection(){
    print_menu
 	
 	get_user_input
 	
 	case $user_input in 
- 1) 
- 	print_list 
- 	;;
- 2) 
-	print_an_index_in_ccurrent_array
- 	;;
- 3) add_an_item_to_array
- 	;;
- 4) remove_last_index
- 	;;
- 5) remove_item_at_x_index
- 	;;
- 6) save_current_array
-    ;;
- 7) load_a_saved_array
-    ;;
- 8) list_saved_arrays
- 	;;
- 9) delete_a_saved_array
-    ;;
- 10)generate_random_box
-    ;;
-    
- 11) quit
-    break
- 	;;
+ 1) print_list ;;
+ 2) print_an_index_in_ccurrent_array ;;
+ 3) add_an_item_to_array ;;
+ 4) remove_last_index ;;
+ 5) remove_item_at_x_index ;;
+ 6) save_current_array ;;
+ 7) load_a_saved_array ;;
+ 8) list_saved_arrays ;;
+ 9) delete_a_saved_array ;;
+ 10) generate_random_box ;;
+ 11) quit;;
     *) echo "Please Enter a Valid Number"
-	menu_selection
-	;;
-	
+	menu_selection ;;
  esac
 }
 
-# Saves the current array to a text file
+# ===============================================
+# Saves current array to a text file in array_storage
+# ===============================================
 save_current_array()
 {
 	echo What is the name of the file you want to save
@@ -155,7 +133,9 @@ save_current_array()
 	menu_selection
 }
 
-# Saves the array and exits the program
+# ===============================================
+# Saves array and exits the program
+# ===============================================
 save_and_leave()
 {
 	echo What is the name of the file you want to save
@@ -166,36 +146,35 @@ save_and_leave()
 	ls "$array_storage" 
 }
 
-# Lists all saved arrays in the data folder
+# ===============================================
+# Lists all saved array files in array_storage
+# ===============================================
 list_saved_arrays()
 {
 	ls "$array_storage"
 	menu_selection
 }
 
-# Asks user to save before quitting
+# ===============================================
+# Asks user if they want to save before quitting
+# ===============================================
 quit()
 {
 	echo Do you want to save before you quit
 	get_user_input
 	case $user_input in
-	  y) 
-	   save_and_leave
-	  ;;
-	  yes) save_and_leave
-	  ;;
-	  no) clear
-	   exit 0
-	  ;;
-	  n) clear
-	   exit 0
-	  ;;
+	  y) save_and_leave ;;
+	  yes) save_and_leave ;;
+	  no) clear; exit 0 ;;
+	  n) clear; exit 0 ;;
 	  *) echo Input Only accepts y/n/yes/no
-	  quit
+	  quit ;;
 	esac
 }
 
-# Deletes a saved array from the data folder
+# ===============================================
+# Deletes a chosen array file from storage
+# ===============================================
 delete_a_saved_array()
 {
 	echo Type name of file you want to delete, type exit to leave
@@ -213,7 +192,9 @@ delete_a_saved_array()
 	menu_selection
 }
 
-# Loads a saved array from file
+# ===============================================
+# Loads a saved array file into Objects
+# ===============================================
 load_a_saved_array()
 { 
 	echo " Input  name of text , type exit to leave"
@@ -236,38 +217,67 @@ load_a_saved_array()
 		load_a_saved_array
 	fi
 }
+
+# ===============================================
+# Prompts the user for how many random objects to generate
+# ===============================================
 get_prompt_size()
 { 
 	read -p "Enter a Number between 1-30 for the array: " box_size
 	if [[ $box_size -ge 0 &&  $box_size -le $maxnum && $box_size =~ [0-9]+$ ]]; then
 		return 0
-		
 	else
 	 echo Invalid Input
 	 generate_random_box
 	fi
-
 }
+
+# ===============================================
+# Randomly selects lines from the warehouse file
+# ===============================================
 load_object_pool()
 {
 	shuf -n $box_size "$datafolder/warehouse_of_objects.txt" > "$datafolder/random_file.txt"
 }
 
-
+# ===============================================
+# Generates a random object box and displays it
+# ===============================================
 generate_random_box()
 {
-
- get_prompt_size
- load_object_pool
-
-	
- mapfile -t loaded_array < "$datafolder/random_file.txt"
- Objects=("${loaded_array[@]}")
- for item in "${!Objects[@]}";  do
+	get_prompt_size
+	load_object_pool
+	mapfile -t loaded_array < "$datafolder/random_file.txt"
+	Objects=("${loaded_array[@]}")
+	for item in "${!Objects[@]}";  do
  		echo "$item) ${Objects[$item]}"
- done
- menu_selection
+	done
+	menu_selection
+}
+remove_item_at_x_index()
+{
+	echo what Item do you want to remove
+	get_user_input
+	if [[ $user_input =~ [0-9]+$ && $user_input -le ${#Objects[@]} && $user_input -ge 0 ]]; then
+		echo  "${Objects[$user_input]} deleted"
+		unset 'Objects[$user_input]'
+		Objects=("${Objects[@]}")
+		echo "Showing List
+		=====================
+		"
+		for item in "${!Objects[@]}";  do
+ 		echo "$item) ${Objects[$item]}"
+	done
+    else
+	{
+		echo Input Valid index
+		remove_item_at_x_index
+	}
+	fi
+	menu_selection
 }
 
-# Starts the program by showing the menu
+# ===============================================
+# Entry point: Starts the program by showing the menu
+# ===============================================
 menu_selection
